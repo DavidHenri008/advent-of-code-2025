0cfadef7486709df2b7b0073bacc07ba3e7eab5b
David Henri <dxh06@acuitysso.com>
2025-12-06 18:24:08 -0500

    Init days

---
+++ src/day 3/index.js
@@ -0,0 +1,16 @@
+const { loadInput } = require('../utils');
+
+const input = loadInput(3);
+
+/**** Part ONE ****/
+const part1 = () => {
+
+};
+
+/**** Part TWO ****/
+const part2 = () => {
+
+};
+
+part1();
+part2();
---
+++ src/day 4/index.js
@@ -0,0 +1,16 @@
+const { loadInput } = require('../utils');
+
+const input = loadInput(4);
+
+/**** Part ONE ****/
+const part1 = () => {
+
+};
+
+/**** Part TWO ****/
+const part2 = () => {
+
+};
+
+part1();
+part2();
